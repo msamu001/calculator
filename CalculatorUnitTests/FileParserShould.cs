086fa1ce63a521d463352134b0378dcfc045bf42
Marcellino Samuels <Marcellino.Samuels@navico.com>
2019-07-16 18:16:43 +0100

    Refractor Parser test for addition

--- CalculatorUnitTests/FileParserShould.cs
+++ CalculatorUnitTests/FileParserShould.cs
@@ -7,27 +7,28 @@ namespace CalculatorUnitTests
     public class FileParserShould
     {
         [Theory]
-        [InlineData("apply 3","3")]
-        [InlineData("apply 7","7")]
-        [InlineData("apply 10","10")]
+        [InlineData("apply 3", "3")]
+        [InlineData("apply 7", "7")]
+        [InlineData("apply 10", "10")]
         public void ReturnApplyNumberAsAString(string input, string expectedResult)
         {
             var parser = new FileParser();
 
             var result = parser.Parse(input);
-            
+
             result.Should().Be(expectedResult);
         }
 
-        [Fact]
-        public void ReturnAddNumberAsCorrectSymbolAndNumberString()
+        [Theory]
+        [InlineData("add 2", "+2")]
+        [InlineData("add 20", "+20")]
+        public void ReturnAddNumberAsCorrectSymbolAndNumberString(string input, string expectedResult)
         {
-            var input = "add 2";
             var parser = new FileParser();
 
             var result = parser.Parse(input);
 
-            result.Should().Be("+2");
+            result.Should().Be(expectedResult);
         }
     }
 }

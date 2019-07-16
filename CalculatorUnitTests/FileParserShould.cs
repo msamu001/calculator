e74f4327c88b2e0c0bf3299c62f55a2156a13400
Marcellino Samuels <Marcellino.Samuels@navico.com>
2019-07-16 18:54:02 +0100

    Refactored tests

--- CalculatorUnitTests/FileParserShould.cs
+++ CalculatorUnitTests/FileParserShould.cs
@@ -8,57 +8,14 @@ namespace CalculatorUnitTests
     {
         [Theory]
         [InlineData("apply 3", "3")]
-        [InlineData("apply 7", "7")]
         [InlineData("apply 10", "10")]
-        public void ReturnApplyAndNumberAsAString(string input, string expectedResult)
-        {
-            var parser = new FileParser();
-
-            var result = parser.Parse(input);
-
-            result.Should().Be(expectedResult);
-        }
-
-        [Theory]
         [InlineData("add 2", "+2")]
         [InlineData("add 20", "+20")]
-        public void ReturnAddAndNumberAsCorrectSymbolAndNumberString(string input, string expectedResult)
-        {
-            var parser = new FileParser();
-
-            var result = parser.Parse(input);
-
-            result.Should().Be(expectedResult);
-        }
-        
-        [Theory]
         [InlineData("subtract 4", "-4")]
         [InlineData("subtract 44", "-44")]
-        public void ReturnSubtractAndNumberAsCorrectSymbolAndNumberString(string input, string expectedResult)
-        {
-            var parser = new FileParser();
-
-            var result = parser.Parse(input);
-
-            result.Should().Be(expectedResult);
-        }
-        
-        [Theory]
-        [InlineData("divide 6", "/6")]
-        [InlineData("divide 12", "/12")]
-        public void ReturnDivideAndNumberAsCorrectSymbolAndNumberString(string input, string expectedResult)
-        {
-            var parser = new FileParser();
-
-            var result = parser.Parse(input);
-
-            result.Should().Be(expectedResult);
-        }
-        
-        [Theory]
         [InlineData("multiply 3", "*3")]
         [InlineData("multiply 27", "*27")]
-        public void ReturnMultiplyAndNumberAsCorrectSymbolAndNumberString(string input, string expectedResult)
+        public void ReturnOneLineOfInputAsCorrectSymbolAndNumber(string input, string expectedResult)
         {
             var parser = new FileParser();
 
